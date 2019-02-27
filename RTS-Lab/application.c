f8d5d25a3c57c847d1968629ea4274b1bb7b7624
Magnus Brattlöf <brattlof@net.chalmers.se>
2019-02-27 11:50:23 +0100

    2019-02-27

--- RTS-Lab/application.c
+++ RTS-Lab/application.c
@@ -4,9 +4,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define START 0
+#define STOP 1
+#define KEY 2
+#define TEMPO 3
+#define VOLUME 4
 
 char *DAC_OUTPUT = (char *) 0x4000741C;
-
+int LEADER = 0;
 int freqind[32] = {0, 2, 4, 0, 0, 2, 4, 0, 4, 5, 7, 4, 5, 7, 7, 9, 7, 5, 4, 0, 7, 9, 7, 5, 4, 0, 0, -5, 0, 0, -5, 0};
 int periods[25] = {2024, 1908, 1805, 1701, 1608, 1515, 1433, 1351, 1276, 1205, 1136, 1073, 1012, 956, 903, 852, 804, 759, 716, 676, 638, 602, 568, 536, 506};
 float beats[32] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 0.5, 0.5, 0.5, 0.5, 1, 1, 0.5, 0.5, 0.5, 0.5, 1, 1, 1, 1, 2, 1, 1, 2};
@@ -40,11 +45,12 @@ typedef struct {
     int tempo;
 	int count;
 	int killed;
+	int started;
 } Melody;
 
 App app = { initObject(), 0, 0, 0, '\0' };
 
-Sound s = { initObject(), 1136, 5, 0, 0, 100, 100, 0 };
+Sound s = { initObject(), 1136, 10, 0, 0, 100, 100, 0 };
 Sound b = { initObject(), 1300, 5, 0, 1000, 1300, 1300 };
 Melody m = { initObject(), 120, 0};
 
@@ -65,56 +71,148 @@ void unkill(Sound *self, int unused);
 void unkill_player(Melody *self, int unused);
 void kill_player(Melody *self, int unused);
 void melody_player(Melody *self, int unused);
+void prepare_can_msg(CANMsg *self, int value);
+void start_player(Melody *self, int unused);
 
 Serial sci0 = initSerial(SCI_PORT0, &app, reader);
 
 Can can0 = initCan(CAN_PORT0, &app, receiver);
 
+void start_player(Melody *self, int unused) {
+	unkill_player(self, 0);
+	if(self->started) {
+		ASYNC(self, melody_player, 0);
+	}
+	else {
+		ASYNC(&s, tone_generator, 0);
+		ASYNC(self, melody_player, 0);
+	}
+}
+
 void receiver(App *self, int unused) {
     CANMsg msg;
     CAN_RECEIVE(&can0, &msg);
-	
-	if(msg.buff[0] == 'p') {
-		SCI_WRITE(&sci0, "Can msg received: ");
-		SCI_WRITE(&sci0, msg.buff[0]);
-		unkill_player(&m, 0);
-		ASYNC(&m, melody_player, 0);
-		ASYNC(&s, tone_generator, 0);
+	if(!LEADER) {
+		if(msg.msgId == START) {
+			start_player(&m, 0);
+		}
+		else if(msg.msgId == STOP) {
+			kill_player(&m, 0);
+		}
+		else if(msg.msgId == KEY) {
+			int key = msg.buff[0];
+			set_key(&s, key);
+		}
+		else if(msg.msgId == TEMPO) {
+			int tempo = msg.buff[0];
+			set_tempo(&m, tempo);
+		}
+		else if(msg.msgId == VOLUME) {
+			switch(msg.buff[0]) {
+				case 0:
+					volume_control(&s, 'd');
+				case 1:
+					volume_control(&s, 'u');
+				default:
+					SCI_WRITE(&sci0, "Blabla ");
+			}
+			SCI_WRITE(&sci0, "Can msg received: ");
+			SCI_WRITECHAR(&sci0, msg.msgId);
+			SCI_WRITE(&sci0,"\n");
+		}
 	}
-	else if(msg.buff[0] == 's') {
-		SCI_WRITE(&sci0, "Can msg received: ");
-		SCI_WRITE(&sci0, msg.buff[0]);
-		kill_player(&m, 0);
-	}
-   
 }
 
 void reader(App *self, int c) {
     //SCI_WRITE(&sci0, "Rcv: \'");
 	SCI_WRITECHAR(&sci0, c);
+	SCI_WRITE(&sci0,"\n");
 	CANMsg msg;
-	if(c == 't'){
-
+	
+	if(c == 't') {
 		self->inpStr[self->count] = '\0';
 		int tempo = atoi(self->inpStr);
-		//self->runSum = self->runSum + self->myNum;
-		//snprintf(self->bufNum, 50, "%d\n", self->myNum);
-		//snprintf(self->bufSum, 50, "%d\n", self->runSum);
-
-		//SCI_WRITE(&sci0, "\n");
-
-		//SCI_WRITE(&sci0,"The entered number is: ");
-		//SCI_WRITE(&sci0, self->bufNum);
-		//SCI_WRITE(&sci0,"\n");
-
-		//SCI_WRITE(&sci0,"The running number is: ");
-		//SCI_WRITE(&sci0,self->bufSum);
-		//SCI_WRITE(&sci0,"\n");
-
-		//int tempo = atoi(self->myNum);
-		SYNC(&m, tempo_control, tempo);
-		//tempo_control(&m, tempo);
 		self->count = 0;
+		
+		if(tempo > 59 && tempo < 100) {
+			msg.buff[0] = self->inpStr[0];
+			msg.buff[1] = self->inpStr[1];
+			msg.buff[2] = 0;
+			msg.length = 3;
+		}
+		else if(tempo >= 100 && tempo <= 250) {
+			msg.buff[0] = self->inpStr[0];
+			msg.buff[1] = self->inpStr[1];
+			msg.buff[2] = self->inpStr[2];
+			msg.buff[3] = 0;
+			msg.length = 4;
+		}
+		
+		msg.msgId = '3';
+		msg.nodeId = 1;
+		//SYNC(&m, tempo_control, tempo);
+		CAN_SEND(&can0, &msg);
+		
+		if(LEADER) {
+			set_tempo(&m, tempo);
+		}
+	}
+	
+	else if (c == 'k') {
+		
+		self->inpStr[self->count] = '\0';
+		int key = atoi(self->inpStr);
+
+		if(key < 0) {
+			msg.buff[0] = self->inpStr[0];
+			msg.buff[1] = self->inpStr[1];
+			msg.buff[2] = 0;
+			msg.length = 3;
+		}
+		else {
+			msg.buff[0] = self->inpStr[0];
+			msg.buff[1] = 0;
+			msg.length = 2;
+		}
+		
+		msg.msgId = '4';
+		msg.nodeId = 1;
+		self->count = 0;
+		CAN_SEND(&can0, &msg);
+		if(LEADER) {
+			set_key(&s, key);
+		}
+	}
+	else if(c == 'p') {
+		
+		msg.msgId = '5';
+		msg.nodeId = 1;
+		msg.length = 1;
+		msg.buff[0] = 0;
+		CAN_SEND(&can0, &msg);
+		if(LEADER) {
+			start_player(&m, 0);
+		}
+	}
+	else if(c == 's') {
+
+		msg.msgId = '6';
+		msg.nodeId = 1;
+		msg.length = 1;
+		msg.buff[0] = 0;
+		CAN_SEND(&can0, &msg);
+		if(LEADER) {
+			kill_player(&m, 0);
+		}
+	}
+
+	else if(c == 'l') {
+		if(LEADER) {
+			LEADER = 0;
+		}
+		else {
+			LEADER = 1;
+		}
 	}
 
 	else if(c == 'F') {
@@ -122,7 +220,9 @@ void reader(App *self, int c) {
 		SCI_WRITE(&sci0, "<The running sum is 0\n");
 	}
 	else if(c == 'u' || c == 'd' || c == 'm') {
-		SYNC(&s, volume_control, c);
+		if(LEADER) {
+			SYNC(&s, volume_control, c);
+		}
 		//volume_control(&s, c);
 	}
 
@@ -136,29 +236,7 @@ void reader(App *self, int c) {
         //deadline_control(&s, c);
         //deadline_control(&b, c);
     }
-	else if (c == 'k') {
-		self->inpStr[self->count] = '\0';
-		int key = atoi(self->inpStr);
 
-		set_key(&s, key);
-		self->count = 0;
-	}
-	else if(c == 'p') {
-		msg.msgId = 1;
-		msg.nodeId = 1;
-		msg.length = 2;
-		msg.buff[0] = 'p';
-		msg.buff[1] = 0;
-		CAN_SEND(&can0, &msg);
-	}
-	else if(c == 's') {
-		msg.msgId = 1;
-		msg.nodeId = 1;
-		msg.length = 2;
-		msg.buff[0] = 's';
-		msg.buff[1] = 0;
-		CAN_SEND(&can0, &msg);
-	}
 	else {
 		self->inpStr[self->count] = c;
 		self->count++;
@@ -248,7 +326,6 @@ void tone_generator(Sound *self, int unused) {
 			*DAC_OUTPUT = self->volume;
 		}
 	}
-
 	AFTER(USEC(self->period), self, tone_generator, 0);
 }
 void set_period(Sound *self, int p){
@@ -256,7 +333,7 @@ void set_period(Sound *self, int p){
 }
 	
 void set_key(Sound *self, int k){
-	self->key = k;
+	self->key = k - 5;
 }
 
 void set_tempo(Melody *self, int t) {
@@ -265,14 +342,18 @@ void set_tempo(Melody *self, int t) {
 
 void melody_player(Melody *self, int unused) {
 
-    // Disable killing of tone
-    unkill(&s, 0);
-	set_period(&s, self->count);
+	self->started = 1;
 	
-    // After the beat period - 50 kill the current tone
-    AFTER(MSEC((60000/self->tempo) * beats[self->count] - 100), &s, kill_tone, 0);
-
 	if(!self->killed) {
+		
+		// Disable killing of tone
+		unkill(&s, 0);
+		set_period(&s, self->count);
+		
+		// After the beat period - 50 kill the current tone
+		AFTER(MSEC((60000/self->tempo) * beats[self->count] - 50), &s, kill_tone, 0);
+
+	
 		// After beat period call melody_player again but with new tone
 		AFTER(MSEC((60000/self->tempo) * beats[self->count]), self, melody_player, 0);
 		// Increment the counting of tone indices
@@ -285,16 +366,13 @@ void unkill(Sound *self, int unused) {
 }
 
 void kill_tone(Sound *self, int unused) {
-	if(self->killed) {
-		self->killed = 0;
-	}
-	else {
-		self->killed = 1;
-	}
+	self->killed = 1;
 }
 
 void kill_player(Melody *self, int unused) {
 	self->killed = 1;
+	self->count = 0;
+	kill_tone(&s, 0);
 }
 
 void unkill_player(Melody *self, int unused) {
